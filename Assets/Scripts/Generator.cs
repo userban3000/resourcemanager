ff3bd980a7ff76a7a25db2498cca7bc3974b33bd
userban3000 <74932693+userban3000@users.noreply.github.com>
2021-01-10 23:42:19 +0200

    Initial commit

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+    
+    [Header("Properties")]
+    public float workToGenerate;
+    public float workPerTick;
+
+    [Header("Internals")]
+    public bool isActive;
+    public float work;
+    public float energy;
+
+    private void Update() {
+        if ( Input.GetButtonDown("Fire1") ) {
+            isActive = !isActive;
+        }
+    }
+
+    private void FixedUpdate() {
+        
+        //if machine is off, don't modify anthing
+        if ( !isActive )
+            return;
+
+        if ( work == workToGenerate ) {
+            Generate();
+            work = 0;
+        } else {
+            work += workPerTick;
+        }
+
+    }
+
+    public void Generate() {
+        Debug.Log("ia deaici ca nai servici");
+    }
+
+}
